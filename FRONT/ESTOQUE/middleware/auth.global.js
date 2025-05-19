83c60ee950217727b1c9c1c83c4b371100134e00
Emb-SuporteTI04 <vitoroll696@gmail.com>
2025-05-19 14:04:45 -0300

    a

--- FRONT/ESTOQUE/middleware/auth.global.js
+++ FRONT/ESTOQUE/middleware/auth.global.js
@@ -1,9 +1,9 @@
+// middleware/auth.global.js
 export default defineNuxtRouteMiddleware((to, from) => {
-  // Executa no client-side, então use process.client para acessar localStorage
   if (process.client) {
-    const autorizado = localStorage.getItem('loginAutorizado') === 'true';
+    const autorizado = localStorage.getItem('loginAutorizado') === 'true'
     if (!autorizado && to.path !== '/login') {
-      return navigateTo('/login');
+      return navigateTo('/login')
     }
   }
-});
+})
