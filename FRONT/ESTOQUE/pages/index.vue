3f91ec774ba2f5e0be7da17abfb3da3e96547a64
Emb-SuporteTI04 <vitoroll696@gmail.com>
2025-05-19 13:56:14 -0300

    não mostra nada se não tiver logado

--- FRONT/ESTOQUE/pages/index.vue
+++ FRONT/ESTOQUE/pages/index.vue
@@ -1,27 +1,37 @@
-<template>
-  <h1 style="font-size: 7rem;">DICIONÁRIO DE DADOS</h1>
-  <iframe 
-    src="https://apiprojifsp.azurewebsites.net/MasterData/Form/Render/produto" 
-    style="width: 100%; height: 80vh; border: none;"
-  ></iframe>
-</template>
-
-
 <script setup>
 import { ref, onMounted } from 'vue'
 
 const htmlAdjusted = ref('')
+const loginAutorizado = ref(false)
 
 onMounted(async () => {
-  const res = await fetch('/api/DataDictionary/Element/Index') // proxy no nuxt.config.ts para https://apiprojifsp.azurewebsites.net
-  let htmlFromApi = await res.text()
+  // Verifica se o login foi autorizado
+  loginAutorizado.value = localStorage.getItem('loginAutorizado') === 'true'
 
-  htmlAdjusted.value = htmlFromApi
-    .replaceAll('/_content/JJMasterData.Web/css/', '/data-dictionary/css/')
-    .replaceAll('/_content/JJMasterData.Web/js/', '/data-dictionary/js/')
-    .replaceAll('/_content/JJMasterData.Web/images/', '/data-dictionary/images/')
+  // Só busca os dados se estiver autorizado
+  if (loginAutorizado.value) {
+    const res = await fetch('/api/DataDictionary/Element/Index') // proxy no nuxt.config.ts
+    const htmlFromApi = await res.text()
+
+    htmlAdjusted.value = htmlFromApi
+      .replaceAll('/_content/JJMasterData.Web/css/', '/data-dictionary/css/')
+      .replaceAll('/_content/JJMasterData.Web/js/', '/data-dictionary/js/')
+      .replaceAll('/_content/JJMasterData.Web/images/', '/data-dictionary/images/')
+  }
 })
 </script>
+
+
+<template>
+  <div v-show="loginAutorizado">
+    <h1 style="font-size: 7rem;">DICIONÁRIO DE DADOS</h1>
+    <iframe 
+      src="https://apiprojifsp.azurewebsites.net/MasterData/Form/Render/produto" 
+      style="width: 100%; height: 80vh; border: none;"
+    ></iframe>
+  </div>
+</template>
+
 
 <style scoped>
 .dictionary-container {
