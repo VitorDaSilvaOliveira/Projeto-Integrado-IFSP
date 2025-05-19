82b574093ea8f82cdb6a6bd57ea578e9a1ddb53e
Emb-SuporteTI04 <vitoroll696@gmail.com>
2025-05-19 14:04:45 -0300

    a

--- FRONT/ESTOQUE/pages/index.vue
+++ FRONT/ESTOQUE/pages/index.vue
@@ -1,37 +1,41 @@
-<script setup>
-import { ref, onMounted } from 'vue'
+<script>
+export default {
+  data () {
+    return {
+      htmlAdjusted: '',
+      loginAutorizado: false
+    }
+  },
 
-const htmlAdjusted = ref('')
-const loginAutorizado = ref(false)
+  async mounted () {
+ 
+    this.loginAutorizado = localStorage.getItem('loginAutorizado') === 'true'
 
-onMounted(async () => {
-  // Verifica se o login foi autorizado
-  loginAutorizado.value = localStorage.getItem('loginAutorizado') === 'true'
+    if (this.loginAutorizado) {
+      const res = await fetch('/api/DataDictionary/Element/Index') // proxy no nuxt.config.ts
+      const htmlFromApi = await res.text()
 
-  // Só busca os dados se estiver autorizado
-  if (loginAutorizado.value) {
-    const res = await fetch('/api/DataDictionary/Element/Index') // proxy no nuxt.config.ts
-    const htmlFromApi = await res.text()
+      this.htmlAdjusted = htmlFromApi
+        .replaceAll('/_content/JJMasterData.Web/css/', '/data-dictionary/css/')
+        .replaceAll('/_content/JJMasterData.Web/js/', '/data-dictionary/js/')
+        .replaceAll('/_content/JJMasterData.Web/images/', '/data-dictionary/images/')
+    }
 
-    htmlAdjusted.value = htmlFromApi
-      .replaceAll('/_content/JJMasterData.Web/css/', '/data-dictionary/css/')
-      .replaceAll('/_content/JJMasterData.Web/js/', '/data-dictionary/js/')
-      .replaceAll('/_content/JJMasterData.Web/images/', '/data-dictionary/images/')
   }
-})
+
+}
 </script>
 
 
 <template>
-  <div v-show="loginAutorizado">
+  <div v-if="loginAutorizado">
     <h1 style="font-size: 7rem;">DICIONÁRIO DE DADOS</h1>
     <iframe 
-      src="https://apiprojifsp.azurewebsites.net/MasterData/Form/Render/produto" 
-      style="width: 100%; height: 80vh; border: none;"
+    src="https://apiprojifsp.azurewebsites.net/MasterData/Form/Render/produto" 
+    style="width: 100%; height: 80vh; border: none;"
     ></iframe>
   </div>
 </template>
-
 
 <style scoped>
 .dictionary-container {
