6ed4ea7be9e647b3bada7af8598278183bfe4858
VitorDaSilvaOliveira <suporte_ti4@grupoembalarte.com.br>
2025-05-18 23:33:04 -0300

    iframe

--- FRONT/ESTOQUE/pages/index.vue
+++ FRONT/ESTOQUE/pages/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    oii
-  </div>
+  <h1 style="font-size: 7rem;">DICIONÁRIO DE DADOS</h1>
+  <iframe 
+    src="https://apiprojifsp.azurewebsites.net/DataDictionary/Element/Index" 
+    style="width: 100%; height: 90vh; border: none;"
+  ></iframe>
 </template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const htmlAdjusted = ref('')
+
+onMounted(async () => {
+  const res = await fetch('/api/DataDictionary/Element/Index') // proxy no nuxt.config.ts para https://apiprojifsp.azurewebsites.net
+  let htmlFromApi = await res.text()
+
+  htmlAdjusted.value = htmlFromApi
+    .replaceAll('/_content/JJMasterData.Web/css/', '/data-dictionary/css/')
+    .replaceAll('/_content/JJMasterData.Web/js/', '/data-dictionary/js/')
+    .replaceAll('/_content/JJMasterData.Web/images/', '/data-dictionary/images/')
+})
+</script>
+
+<style scoped>
+.dictionary-container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 1rem;
+  border: 1px solid #ddd;
+  overflow-x: auto;
+}
+</style>
