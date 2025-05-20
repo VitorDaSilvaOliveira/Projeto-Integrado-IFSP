8621b86695523be3e532698fa6c1e3f99b369d14
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-05-20 01:23:34 -0300

    refactor: reestruturado projeto para ASP.NET MVC, removido Nuxt

---
+++ src/Estoque.Infrastructure/Services/ProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using JJMasterData.Core.UI.Components;
+
+namespace Estoque.Infrastructure.Services;
+
+public class ProdutoService(IComponentFactory componentFactory)
+{
+    public async Task<JJFormView> GetFormViewProdutoAsync()
+    {
+        var formView = await componentFactory.FormView.CreateAsync("produto");
+        formView.ShowTitle = true;
+        return formView;
+    }
+}
---
+++ src/Estoque.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+﻿using Estoque.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estoque.Web.Controllers;
+
+public class ProdutoController(ProdutoService produtoService) : Controller
+{
+    [Route("Produto")]
+    public async Task<IActionResult> Produto()
+    {
+        var formViewProdutoAsync = await produtoService.GetFormViewProdutoAsync();
+        
+        var resultGridProdutoAsync = await formViewProdutoAsync.GetResultAsync();
+
+        if (resultGridProdutoAsync is IActionResult actionResultGridProdutoAsync)
+            return actionResultGridProdutoAsync;
+        
+        ViewBag.FormViewProduto = resultGridProdutoAsync.Content;
+        return View();
+    }
+}
