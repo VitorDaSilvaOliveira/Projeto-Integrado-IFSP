52fdccb7159da8fec2240431499246073c03584a
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-05-20 01:27:38 -0300

    Removido front

--- FRONT/ESTOQUE/middleware/auth.global.js
+++
@@ -1,9 +0,0 @@
-// middleware/auth.global.js
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (process.client) {
-    const autorizado = localStorage.getItem('loginAutorizado') === 'true'
-    if (!autorizado && to.path !== '/login') {
-      return navigateTo('/login')
-    }
-  }
-})
