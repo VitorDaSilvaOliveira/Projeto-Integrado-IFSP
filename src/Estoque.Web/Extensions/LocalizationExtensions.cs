decc03b75888dc6834a20d71e98ccae5458a6022
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-05-20 12:23:10 -0300

    fix: corrige problema de cultura em produção (pt-BR forçado via middleware)

---
+++ src/Estoque.Web/Extensions/LocalizationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+
+namespace Estoque.Web.Extensions;
+
+public static class LocalizationExtensions
+{
+    public static void AddPtBrLocalization(this IServiceCollection services)
+    {
+        var supportedCultures = new[] { new CultureInfo("pt-BR") };
+
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            options.DefaultRequestCulture = new RequestCulture("pt-BR");
+            options.SupportedCultures = supportedCultures;
+            options.SupportedUICultures = supportedCultures;
+        });
+    }
+
+    public static void UsePtBrLocalization(this IApplicationBuilder app)
+    {
+        var options = app.ApplicationServices.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+        app.UseRequestLocalization(options);
+    }
+}
