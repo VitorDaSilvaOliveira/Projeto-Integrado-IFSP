7405ccca46e9fcca95657bd409c53050aec54827
Khalilkma <147070933+Khalilkma@users.noreply.github.com>
2025-05-29 15:22:18 -0300

    Configurando o Identity e arrumando a classe Usuario

--- src/Estoque.Web/Program.cs
+++ src/Estoque.Web/Program.cs
@@ -1,6 +1,13 @@
+using Estoque.Domain.Contexts;
+using Estoque.Domain.Models;
 using Estoque.Web.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Testando api com SwashBucle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Serviços
 builder.Services.AddControllersWithViews();
@@ -8,6 +15,18 @@ builder.Services.AddCustomServices();
 builder.Services.AddEstoqueServices();
 builder.Services.AddPtBrLocalization();
 var app = builder.Build();
+
+// Identity
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<Usuario>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+app.MapIdentityApi<Usuario>();
 
 app.UsePtBrLocalization();
 
@@ -26,5 +45,11 @@ app.MapCustomEndpoints();
 // Seeding
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 await app.UseSeedingAsync(logger);
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapSwagger();
+
 
 app.Run();
