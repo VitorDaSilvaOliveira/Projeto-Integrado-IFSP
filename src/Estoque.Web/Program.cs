52febcb7c57427df17914cc8c55d96d997cca531
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-05-29 18:07:02 -0300

    Fix Program.cs

--- src/Estoque.Web/Program.cs
+++ src/Estoque.Web/Program.cs
@@ -20,6 +20,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddCustomServices();
 builder.Services.AddEstoqueServices();
 builder.Services.AddPtBrLocalization();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => { options.SignIn.RequireConfirmedAccount = true; }
+).AddEntityFrameworkStores<EstoqueDbContext>().AddDefaultTokenProviders();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
@@ -27,9 +31,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => { options.SignIn.RequireConfirmedAccount = true; }
-).AddEntityFrameworkStores<EstoqueDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
