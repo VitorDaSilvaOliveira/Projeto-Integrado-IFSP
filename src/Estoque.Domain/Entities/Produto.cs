14fa37976adb6f4bb1a5ce81e5b437e3dc8ca9ff
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-05-29 17:55:03 -0300

    EF Core e Identity

---
+++ src/Estoque.Domain/Entities/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Estoque.Domain.Entities;
+ 
+[Table("tb_produto")]
+public class Produto
+{
+    [Key] [Column("id_produto")] public int IdProduto { get; set; }
+
+    [Column("nome")] [StringLength(150)] public string? Nome { get; set; }
+
+    [Column("descricao")]
+    [StringLength(200)]
+    public string? Descricao { get; set; }
+
+    [Column("preco", TypeName = "decimal(20,2)")]
+    public decimal? Preco { get; set; }
+
+    [Column("quantidade_estoque")] public int? QuantidadeEstoque { get; set; }
+
+    [Column("id_categoria")] public int? IdCategoria { get; set; }
+}
--- src/Estoque.Domain/Models/Usuario.cs
+++ src/Estoque.Domain/Models/Usuario.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Estoque.Domain.Models
-{
-    [Table("Usuario")]
-    public class Usuario : IdentityUser
-    {
-        [Key]
-        [Column("id_usuario")]
-        public override string Id { get; set; } = Guid.NewGuid().ToString();
-
-        [Required]
-        [Column("nome")]
-        public string Nome { get; set; }
-
-        [Required]
-        [Column("email")]
-        public override string Email { get; set; }
-
-        [Required]
-        [Column("senha_hash")]
-        public string SenhaHash { get; set; }
-
-        [Required]
-        [Column("tipo_usuario")]
-        public string TipoUsuario { get; set; }
-    }
-}
+﻿// using Microsoft.AspNetCore.Identity;
+// using System.ComponentModel.DataAnnotations;
+// using System.ComponentModel.DataAnnotations.Schema;
+//
+// namespace Estoque.Domain.Models
+// {
+//     [Table("Usuario")]
+//     public class Usuario : IdentityUser
+//     {
+//         [Key]
+//         [Column("id_usuario")]
+//         public override string Id { get; set; } = Guid.NewGuid().ToString();
+//
+//         [Required]
+//         [Column("nome")]
+//         public string Nome { get; set; }
+//
+//         [Required]
+//         [Column("email")]
+//         public override string Email { get; set; }
+//
+//         [Required]
+//         [Column("senha_hash")]
+//         public string SenhaHash { get; set; }
+//
+//         [Required]
+//         [Column("tipo_usuario")]
+//         public string TipoUsuario { get; set; }
+//     }
+// }
