b125a4cd1bcbb796d873e16814aaa998d5015cc7
RValverdeZS <r.valverde@aluno.ifsp.edu.br>
2025-06-06 16:06:14 -0300

    Implementação do Logout

---
+++ src/Estoque.Web/Areas/Estoque/Controllers/SignOutController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Estoque.Domain.Entities;
+
+namespace Estoque.Web.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class SignOutController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public SignOutController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            await _signInManager.SignOutAsync(); 
+            return RedirectToAction("Index", "SignIn"); 
+        }
+    }
+}
