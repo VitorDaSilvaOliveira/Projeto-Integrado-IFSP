0b94374ff913289b7a6aa6942b0b61f5a84f4439
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-06-06 16:43:16 -0300

    SignOutController.cs para a área Identity

---
+++ src/Estoque.Web/Areas/Identity/Controllers/SignOutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Estoque.Domain.Entities;
+
+namespace Estoque.Web.Areas.Identity.Controllers;
+
+[Area("Identity")]
+public class SignOutController(SignInManager<ApplicationUser> signInManager) : Controller
+{
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index()
+    {
+        await signInManager.SignOutAsync();
+        return RedirectToAction("Index", "SignIn");
+    }
+}
