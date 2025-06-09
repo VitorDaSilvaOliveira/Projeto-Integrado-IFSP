c2c8fc2dadcb1bb21238bf8a13278ff33653e88d
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-06-09 17:33:18 -0300

    Area Admin e Log

---
+++ src/Estoque.Infrastructure/Services/LogService.cs
@@ -0,0 +1,13 @@
+﻿using JJMasterData.Core.UI.Components;
+
+namespace Estoque.Infrastructure.Services;
+
+public class LogService(IComponentFactory componentFactory)
+{
+    public async Task<JJFormView> GetFormViewLogAsync()
+    {
+        var formView = await componentFactory.FormView.CreateAsync("Log");
+        formView.ShowTitle = true;
+        return formView;
+    }
+}
