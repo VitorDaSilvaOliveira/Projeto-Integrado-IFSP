bb3646fb92b983586680d18e360f1b2206cb7249
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-06-10 13:25:25 -0300

    Gerenciador de Usuários

---
+++ src/Estoque.Domain/Models/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Estoque.Domain.Models;
+
+public class UserViewModel
+{
+    [Required(ErrorMessage = "O campo Nome de Usuário é obrigatório.")]
+    public string Username { get; set; }
+
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
