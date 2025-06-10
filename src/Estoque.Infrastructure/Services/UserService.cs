1bbf740496f70d9ecaf5399b466d18c895754321
Khalilkma <147070933+Khalilkma@users.noreply.github.com>
2025-06-10 13:39:52 -0300

    Revert "Merge branch 'master' of https://github.com/VitorDaSilvaOliveira/Projeto-Integrado-IFSP"
    This reverts commit 0b4b0d4cafdf3119cc8c9c99644aeb69b992817b, reversing
    changes made to 962e3410a8d16f51f737a8f037efadde829c0d8b.

--- src/Estoque.Infrastructure/Services/UserService.cs
+++ src/Estoque.Infrastructure/Services/UserService.cs
@@ -1,30 +1,10 @@
 ﻿using Estoque.Infrastructure.Data;
-using JJMasterData.Core.DataDictionary;
-using JJMasterData.Core.DataDictionary.Models.Actions;
-using JJMasterData.Core.UI.Components;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Estoque.Infrastructure.Services;
 
-public class UserService(EstoqueDbContext context, IWebHostEnvironment env, IComponentFactory componentFactory)
+public class UserService(EstoqueDbContext context, IWebHostEnvironment env)
 {
-    public async Task<JJFormView> GetFormViewUserAsync()
-    {
-        var formView = await componentFactory.FormView.CreateAsync("Usuarios");
-        formView.ShowTitle = true;
-
-        formView.GridView.ToolbarActions.Add(new UrlRedirectAction
-        {
-            Name = "Adicionar",
-            Icon = IconType.PlusCircle,
-            Text = "Adicionar",
-            ShowAsButton = true,
-            UrlRedirect = "/Admin/User/CreateUser"
-        });
-        
-        return formView;
-    }
-    
     public byte[]? GetUserAvatarBytes(string userId)
     {
         if (string.IsNullOrEmpty(userId))
