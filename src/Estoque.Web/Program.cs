e750ae3520bf4d2a85dbe7f6b41f2641c2322ee1
beatriz.muniz <beatriz.muniz@jjconsulting.com.br>
2025-10-15 17:48:02 -0300

    Remove PedidoListener

--- src/Estoque.Web/Program.cs
+++ src/Estoque.Web/Program.cs
@@ -1,10 +1,8 @@
-using Estoque.Infrastructure.Services;
 using Estoque.Web.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddEstoqueConfiguration();
-builder.Services.AddHostedService<PedidoListenerService>();
 
 var app = builder.Build();
 
