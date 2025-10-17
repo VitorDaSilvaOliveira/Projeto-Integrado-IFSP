0671545b4cd8a770751f7d410617da3ef1d0b810
Matheus Prando Appolinario Barbosa <matheusprandoappolinario@gmail.com>
2025-10-17 16:01:57 -0300

    Criação do Relatório de Devolução
    Criação do Controller e View teste para a aba de relatório de devoluções e trocas

---
+++ src/Estoque.Web/Areas/Estoque/Controllers/RelatorioDevolucaoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Estoque.Web.Areas.Estoque.Controllers
+{
+    [Area("Estoque")]
+    public class RelatorioDevolucaoTrocaController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Apenas para teste
+            var testeDevolucoes = new List<string> { "Produto A", "Produto B", "Produto C" };
+            return View(testeDevolucoes);
+        }
+
+    }
+}
