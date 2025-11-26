06b412f276a01c483af8b5cdc45a01f7c4e48454
Gean Carlos <60990296+geanbandeira@users.noreply.github.com>
2025-11-26 15:31:45 -0300

    Revert "Feature/melhoria testes unitarios"

---
+++ src/Estoque.Tests/E2ETests/PageObjects/BasePage.cs
@@ -0,0 +1,109 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SeleniumExtras.WaitHelpers; 
+using OpenQA.Selenium.Support.Extensions;
+
+namespace Estoque.Tests.E2ETests.PageObjects
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver Driver { get; }
+        protected WebDriverWait Wait { get; }
+        protected string BaseUrl { get; }
+
+        protected BasePage(IWebDriver driver, WebDriverWait wait, string baseUrl)
+        {
+            Driver = driver;
+            Wait = wait;
+            BaseUrl = baseUrl;
+        }
+
+        protected void NavigateTo(string relativeUrl)
+        {
+            Driver.Navigate().GoToUrl($"{BaseUrl}{relativeUrl}");
+        }
+
+        protected IWebElement WaitForElement(By locator, int timeoutSeconds = 20)
+        {
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeoutSeconds));
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+            //return wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+        protected void ExecuteScript(string script, IWebElement element)
+        {
+            ((OpenQA.Selenium.IJavaScriptExecutor)Driver).ExecuteScript(script, element);
+        }
+        protected void Click(By locator)
+        {
+            var element = WaitForElement(locator, (int)Wait.Timeout.TotalSeconds);
+
+            try
+            {
+                element.Click();
+            }
+            catch (OpenQA.Selenium.ElementClickInterceptedException)
+            {
+                ExecuteScript("arguments[0].click();", element);
+            }
+            catch (OpenQA.Selenium.ElementNotInteractableException)
+            {
+                ExecuteScript("arguments[0].click();", element);
+            }
+            catch (OpenQA.Selenium.WebDriverException ex) when (ex.Message.Contains("not clickable"))
+            {
+                ExecuteScript("arguments[0].click();", element);
+            }
+            catch (WebDriverTimeoutException)
+            {
+                Console.WriteLine("Botão 'Entrar' não estava disponível no tempo esperado.");
+            }
+
+
+        }
+
+        protected void Type(By locator, string text)
+        {
+            var element = WaitForElement(locator);
+            element.Clear();
+            element.SendKeys(text);
+        }
+
+        protected string GetText(By locator)
+        {
+            return WaitForElement(locator).Text;
+        }
+
+        protected bool IsElementPresent(By locator)
+        {
+            try
+            {
+                Driver.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        protected bool IsElementVisible(By locator)
+        {
+            try
+            {
+                return Driver.FindElement(locator).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        protected void WaitForPageLoad()
+        {
+            Wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+        }
+    }
+}
---
+++ src/Estoque.Tests/E2ETests/UsuarioActivationTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using FluentAssertions;
+using Xunit;
+
+namespace Estoque.Tests.E2ETests
+{
+    [Collection("Selenium")]
+    public class LoginTests : SeleniumTestBase, IDisposable
+    {
+        private readonly WebDriverWait wait;
+
+        public LoginTests()
+        {
+            wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+        }
+
+        private void TentaLogin(string login, string senha, bool esperaSucesso)
+        {
+            LoginPage.GoToLoginPage();
+
+            Driver.FindElement(By.Id("Login")).Clear();
+            Driver.FindElement(By.Id("Login")).SendKeys(login);
+
+            Driver.FindElement(By.Id("senha")).Clear();
+            Driver.FindElement(By.Id("senha")).SendKeys(senha);
+
+            var botaoEntrar = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector("button.btn.BGC-azul-2")));
+
+            new Actions(Driver).MoveToElement(botaoEntrar).Perform();
+            System.Threading.Thread.Sleep(500); 
+
+            botaoEntrar.Click();
+
+            if (esperaSucesso)
+            {
+                wait.Until(d => !d.Url.Contains("/Identity/SignIn"));
+                Driver.Url.Should().NotContain("/Identity/SignIn", "Usuário ativo deveria conseguir fazer login");
+            }
+            else
+            {
+                wait.Until(d => d.Url.Contains("/Identity/SignIn"));
+                wait.Until(d => d.FindElements(By.CssSelector(".text-danger, .alert-danger")).Count > 0);
+
+                var mensagensErro = Driver.FindElements(By.CssSelector(".text-danger, .alert-danger"));
+                mensagensErro.Should().NotBeEmpty("Deveria exibir mensagem de erro para usuário bloqueado ou inválido");
+            }
+        }
+
+        [Fact]
+        [Trait("E2E", "Login")]
+        public void UsuarioAtivo_DeveConseguirFazerLogin()
+        {
+            try
+            {
+                TentaLogin("Admin", "Admin@123", esperaSucesso: true);
+                TakeScreenshot("Usuario_Ativo_Login_Sucesso");
+            }
+            catch (Exception)
+            {
+                TakeScreenshot("ERRO_Usuario_Ativo");
+                throw;
+            }
+        }
+
+        [Fact]
+        [Trait("E2E", "Login")]
+        public void UsuarioBloqueado_NaoDeveConseguirFazerLogin()
+        {
+            try
+            {
+                TentaLogin("Funcionário", "Mario@123", esperaSucesso: false);
+                TakeScreenshot("Usuario_Bloqueado_Login_Negado");
+            }
+            catch (Exception)
+            {
+                TakeScreenshot("ERRO_Usuario_Bloqueado");
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+        }
+    }
+}
